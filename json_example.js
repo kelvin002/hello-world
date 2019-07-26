ea32ef4171848686ed2622ec5d6b68a1dca017b2
kelvin002 <30874207+kelvin002@users.noreply.github.com>
2019-07-27 00:01:07 +0800

    Create json_example.js

---
+++ json_example.js
@@ -0,0 +1,17 @@
+var contents = {
+  "username":"xyz",
+  "password":"xyz@123",
+  "email":"xyz@xyz.com",
+  "uid": 1100
+};
+
+var jsonContent = JSON.parse(contents);
+
+console.log("User Name:", jsonContent.username);
+console.log("Email:", jsonContent.email);
+console.log("Password:", jsonContent.password);
+
+
+
+
+
